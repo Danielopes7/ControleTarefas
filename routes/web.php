81dcdba2461fea946cc606b54ee318afb1a5c12e
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-04-16 22:37:32 -0300

    criando rota para exportacao

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+Route::get('tarefa/exportacao', 'App\Http\Controllers\TarefaController@exportacao');
 Route::resource('tarefa','App\Http\Controllers\TarefaController')->middleware('verified');
+
 
 Route::get('/mensagem-teste', function(){
     Mail::to('daniel.lopes.amorim@hotmail.com')->send(new MensagemTesteEmail());
